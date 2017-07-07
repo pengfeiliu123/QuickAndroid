5c4ca620cb54161627563c7f87456fa4d3ddca24
lpf <liupf5@lenovo.com>
2017-07-07 09:17:23 +0800

    1.scale light bulb image.

--- playflashlight/src/main/java/com/lpf/playlight/demo/FlashLightActivity.java
+++ playflashlight/src/main/java/com/lpf/playlight/demo/FlashLightActivity.java
@@ -1,21 +1,29 @@
 package com.lpf.playlight.demo;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.MotionEventCompat;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewParent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -39,6 +47,11 @@ public class FlashLightActivity extends FragmentActivity implements SharedPrefer
 
     private SeekBar lightSeekBar;
     private ImageView colorChoose;
+    private LinearLayout flashLightLayout;
+
+    private float lastDistance = 0;
+    private float currentDistance = 0;
+    private float scaleSize = 1f;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,13 +60,13 @@ public class FlashLightActivity extends FragmentActivity implements SharedPrefer
 
         theButton = (ToggleButton) findViewById(R.id.flashlightButton);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        background = (LinearLayout)findViewById(R.id.background);
+        background = (LinearLayout) findViewById(R.id.background);
         background.setOnLongClickListener(new LongClickListener());
 
         theButton.setTextOn("Off");
         theButton.setTextOff("On");
 
-        lightSeekBar = (SeekBar)findViewById(R.id.light_seekbar);
+        lightSeekBar = (SeekBar) findViewById(R.id.light_seekbar);
         lightSeekBar.setMax(254);
         lightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -74,7 +87,7 @@ public class FlashLightActivity extends FragmentActivity implements SharedPrefer
 
         final ColorPickerDialog colorPickerDialog = new ColorPickerDialog();
         int[] colors = getResources().getIntArray(R.array.color_choose);
-        colorPickerDialog.initialize(R.string.title,colors, colors[0], 5, 2);
+        colorPickerDialog.initialize(R.string.title, colors, colors[0], 5, 2);
         colorPickerDialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
 
             @Override
@@ -83,7 +96,7 @@ public class FlashLightActivity extends FragmentActivity implements SharedPrefer
             }
         });
 
-        colorChoose = (ImageView)findViewById(R.id.color_choose);
+        colorChoose = (ImageView) findViewById(R.id.color_choose);
         colorChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,6 +105,71 @@ public class FlashLightActivity extends FragmentActivity implements SharedPrefer
             }
         });
 
+        flashLightLayout = (LinearLayout) findViewById(R.id.flashLightLayout);
+        background.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //更兼容的方式
+                int actionMasked = MotionEventCompat.getActionMasked(event);
+
+                switch (actionMasked) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        //当触控点多于两个时进行缩放操作
+                        if (event.getPointerCount()>=2){
+                            //设置当前的两点间的距离
+                            setCurrentDistance(event.getX(0),event.getY(0),event.getX(1),event.getY(1));
+                            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) flashLightLayout.getLayoutParams();
+                            //获取用户屏幕的宽度
+                            Point point = new Point();
+                            ((WindowManager)getApplicationContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getSize(point);
+                            int screenSize = point.x;
+                            //当当前距离大于之前距离时，进行放大操作
+                            if (lastDistance <= 0 ){
+                                lastDistance = currentDistance;
+                            } else{
+                                if (currentDistance>lastDistance){
+                                    if(flashLightLayout.getWidth() < screenSize /2){
+                                        scaleSize = 1.05f;
+                                    }else{
+                                        scaleSize = 1f;
+                                    }
+                                    //设置完放大参数后，将当前距离赋值给lastDistance
+                                    lastDistance=currentDistance;
+                                    //当当前距离小于之前距离时，进行缩小操作
+                                } else if (currentDistance<lastDistance){
+                                    //防止图片被缩小的太小，最小不能超过屏幕宽度的1/4
+                                    if (flashLightLayout.getWidth()>screenSize/4){
+                                        scaleSize = 0.95f;
+                                    } else {
+                                        scaleSize = 1f;
+                                    }
+                                    lastDistance=currentDistance;
+                                }
+                                //设置layoutParams进行缩放操作
+                                layoutParams.height = (int) (flashLightLayout.getHeight()*scaleSize);
+                                layoutParams.width = (int) (flashLightLayout.getWidth()*scaleSize);
+                                flashLightLayout.setLayoutParams(layoutParams);
+                            }
+                        }
+                        break;
+
+                    //触摸动作抬起时将两个距离初始化
+                    case MotionEvent.ACTION_UP:
+                        lastDistance = 0;
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private float setCurrentDistance(float x1, float y1, float x2, float y2){
+        currentDistance = (float) Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        return currentDistance;
     }
 
     @Override
@@ -163,10 +241,11 @@ public class FlashLightActivity extends FragmentActivity implements SharedPrefer
         protected Boolean doInBackground(Void... voids) {
             return flash.on();
         }
+
         @Override
         protected void onPostExecute(Boolean success) {
             theButton.setEnabled(true);
-            if (! success) {
+            if (!success) {
                 Toast.makeText(FlashLightActivity.this, "Failed to access camera.", Toast.LENGTH_SHORT).show();
             }
         }
